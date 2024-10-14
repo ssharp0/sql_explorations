2c31ccc13b404466d4640bb4be24a33a0e656bca
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2024-10-13 20:36:31 -0700

    data setup examples

--- ex_pd.py
+++ ex_pd.py
@@ -18,4 +18,31 @@ def gen_df_from_list_of_dicts(data):
 def gen_df_from_lists_of_lists(data, cols):
     df = pd.DataFrame(data, columns=cols)
     return df
-  
+
+
+# dictionary of lists
+d1 = {
+    "Name": ["Bob", "Sam", "Joe", "April"],
+    "Age": [32, 35, 31, 19],
+    "Gender": ["M", "F", "M", "F"],
+    "Height": [165, 130, 190, 160]
+}
+
+# list of dictionaries
+d2 = [
+    {"Name": "Bob", "Age": 32, "Gender": "M"},
+    {"Name": "Sam", "Age": 35, "Gender": "F"},
+    {"Name": "Joe", "Age": 31, "Gender": "M"},
+]
+
+# list of lists
+d3 = [
+    ["Bob", 32, "M"],
+    ["Sam", 35, "F"],
+    ["Joe", 31, "M"]
+]
+# cols for lists of lists
+cols3 = ["Name", "Age", "Gender"]
+
+# np array
+d4 = np.array(d3)
