791e616b34e2e302791f413c4c1f186bf40e229b
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2025-01-13 22:53:44 -0800

    df to sql

--- python/save_sql.py
+++ python/save_sql.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+def df_to_sql(df, output_file, sql_dialect='mysql'):
+    # Convert DataFrame to SQL
+    if sql_dialect == 'mysql':
+        sql = df.to_sql_string(index=False)
+    elif sql_dialect == 'postgresql':
+        sql = df.to_sql_string(index=False, index_label='id')
+    else:
+        raise ValueError("Unsupported SQL dialect")
+
+    # Write SQL to file
+    with open(output_file, 'w') as f:
+        f.write(sql)
+
+# usage
+df = pd.DataFrame({'A': [1, 2, 3], 'B': ['a', 'b', 'c']})
+df_to_sql(df, 'output.sql', sql_dialect='mysql')
