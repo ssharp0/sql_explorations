337d631819e27ce091282ce8cb944a2b0659e904
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2025-01-19 23:24:44 -0800

    connect to db and fetch

--- python/postgre_sql.py
+++ python/postgre_sql.py
@@ -1,1 +1,20 @@
+import psycopg2
 
+conn = psycopg2.connect(
+    dbname="db",
+    user="username",
+    password="password",
+    host="host",
+    port="port"
+)
+
+cur = conn.cursor()
+
+# Execute query
+cur.execute("SELECT * FROM table")
+
+# Fetch results
+results = cur.fetchall()
+
+for row in results:
+    print(row)
