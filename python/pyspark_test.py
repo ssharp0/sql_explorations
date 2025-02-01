1ad311abd605c4a0c85378d3004fd5991d6706f4
Scott Harp <87046544+ssharp0@users.noreply.github.com>
2025-01-31 23:30:54 -0800

    session and connection

--- python/pyspark_test.py
+++ python/pyspark_test.py
@@ -1,1 +1,16 @@
+import os
+from pyspark.sql import SparkSession
+from duckdb import DuckDBPyConnection
+import duckdb
+
+def create_spark_session():
+    """Creates a PySpark session."""
+    return SparkSession.builder \
+        .appName("PySpark-DuckDB Demo") \
+        .config("spark.jars.packages", "org.duckdb:duckdb_jdbc:0.9.0") \
+        .getOrCreate()
+
+def create_duckdb_connection():
+    """Creates a DuckDB connection."""
+    return duckdb.connect(database=':memory:')
 
